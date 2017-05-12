7bdc6e86e969b90035efa86ad18a1179b1abd1f5
Youssef Hajari <youssef@darbtech.net>
2017-05-12 10:12:50 +0100

    [ADD] Ajouter les fichiers de base du projet darb_puthod

---
+++ __openerp__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "darb_puthod",
+
+    'summary': """
+        Darbtech's module to realise modification
+        for Puthod""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Darbtech",
+    'website': "https://darbtech.net",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale', 'account', 
+	            'account_accountant', 'hr', 'mrp'			
+                ],
+
+    # always loaded
+    'data': [
+        
+    ],
+	#'qweb': ['static/src/js/*.js'],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo.xml',
+    ],
+}
