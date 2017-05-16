ad444da910c04d00276b2168abc03206462caf4e
Ahmed BOUNAR <ahmed@darbtech.net>
2017-05-16 18:17:03 +0100

    [ADD] Menus [ADD] Workflows [ADD] Views for FC (fiche chantier)

--- models/team_work.py
+++ models/team_work.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from openerp import fields, models, api, _
-    
+
 class Equipe(models.Model):
+    
     _name ='equipe'
-  
     name = fields.Char('Name',required=True)
-    manager = fields.Char('Manager',required=True) 
-   #ressource_list= fields.One2Many('class_lié', ondelete='cascade', string="Ressource List", required=True)
+    manager = fields.Many2one('hr.employee', string='Manager', index=True, track_visibility='onchange',required=True)
+   #ressource_list= fields.One2Many('class_liï¿½', ondelete='cascade', string="Ressource List", required=True)
