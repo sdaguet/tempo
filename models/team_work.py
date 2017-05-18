f5fab07bd79e0e4a0e4d740c1166a17e106315fc
Youssef Hajari <youssef@darbtech.net>
2017-05-18 12:02:15 +0100

    [ADD] Ajouter le champ fichechantier_ids dans equipe et sur la vue team_work

--- models/team_work.py
+++ models/team_work.py
@@ -4,8 +4,9 @@ import logging
 _logger = logging.getLogger(__name__)
 
 class Equipe(models.Model):
-
+    _inherit = "hr.employee"
     _name ='equipe'
     name = fields.Char('Team Name',required=True)
     manager = fields.Many2one('hr.employee', string='Manager', index=True, track_visibility='onchange',required=True)
+    fichechantier_ids = fields.One2many('fiche.chantier', 'equipe_id', string='Fiche Chantier', required=True)
    #ressource_list= fields.One2Many('class_li�', ondelete='cascade', string="Ressource List", required=True)
