c902f3df660a01edfd7407248f298872e1dae9da
DARBTECH\zoubida <zoubida@darbtech.net>
2017-06-19 16:18:44 +0000

    [ADD] complete website view and add edit button

--- static/src/js/website_puthod.js
+++ static/src/js/website_puthod.js
@@ -38,4 +38,29 @@ odoo.define('darb_puthod.responsivejson', function(require) {
 			}
 		});
     });
+
+
+	$('.edit').on('click', function(ev) {
+        ev.preventDefault();
+        $(this).parents('tr').find(".edit").toggleClass('hidden');
+
+        var vname = document.getElementById('vname');
+        var knames = document.getElementById('kname');
+        var submit_button = document.getElementById('edit');
+        value_input.addEventListener('input', function(){
+        submit_button.href = "/ficheviewer/?vnane=" + vname.value +"/?knane=" + kname.value ;
+        }
+
+		console.log($(this).parents('tr').find(".vname").val());
+		$.ajax({
+			type: "POST",
+			url: "/ficheviewer",
+			async: false,
+			data: JSON.stringify({}),
+			contentType: "application/json",
+			complete: function (data) {
+				console.log(data);
+			}
+		});
+    });
 });
