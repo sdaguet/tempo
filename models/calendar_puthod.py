aff9a2eee88523a4cb5a918e3b684993b5efb3be
DARBTECH\zoubida <zoubida@darbtech.net>
2017-06-30 15:25:57 +0000

    [ADD] DOM-142 calendar js domain for responsable

--- models/calendar_puthod.py
+++ models/calendar_puthod.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
 from openerp import api, fields, models
+import logging
+_logger = logging.getLogger(__name__)
 
 class CalendarPuthod(models.Model):
     _inherit = 'calendar.event'
 
-    user_ids = fields.Many2many(comodel_name="res.users", string="Responsables", compute="_compute_users")
+    partner_p_ids = fields.Many2many(comodel_name="res.partner", string="Responsables", compute="_compute_users")
+    #
+    # psartner_ids = fields.many2many('res.partner', 'calendar_event_res_partner_rel', string='Attendees',
+    #                                 states={'done': [('readonly', True)]})
 
     @api.one
-    @api.depends('user_ids')
+    @api.depends('partner_p_ids')
     def _compute_users(self):
         users = []
         fiches = self.env['fiche.chantier'].search([('user_id','!=', False)])
         for f in fiches:
-            users.append(f.user_id.id)
-        self.user_ids = users
+            users.append(f.user_id.partner_id.id)
+        self.partner_p_ids = users
         print "iciiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"
-        print users
-        print self.user_ids.ids
-        print self.partner_ids
+        print self.partner_p_ids.ids
+        print self.partner_ids.ids
         pass
-
