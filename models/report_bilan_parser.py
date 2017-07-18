5072c8776cafd400fa9cc4c22b423ced2e43f6a3
DARBTECH\zoubida <zoubida@darbtech.net>
2017-07-18 16:12:22 +0100

    [ADD] DOM-82 first table #time 3h

--- models/report_bilan_parser.py
+++ models/report_bilan_parser.py
@@ -13,6 +13,122 @@ class BilanReport(models.AbstractModel):
     _name = 'report.darb_puthod.report_bilan_chantier'
 
     @api.multi
+    def get_sum_product(self, doc):
+        sum_v = 0
+        sum_p_d = 0
+        sum_t = 0
+        sum_ta = 0
+        sum_pl = 0
+        sum_dh = 0
+        sum_pr = 0
+        sum_g = 0
+        sum_a = 0
+        sum_divers = 0
+        sum_f = 0
+        sum_s = 0
+
+        sum_c_v = 0
+        sum_c_p_d = 0
+        sum_c_t = 0
+        sum_c_ta = 0
+        sum_c_pl = 0
+        sum_c_dh = 0
+        sum_c_pr = 0
+        sum_c_g = 0
+        sum_c_a = 0
+        sum_c_divers = 0
+        sum_c_f = 0
+        sum_c_s = 0
+
+        sum = 0
+        sum_c = 0
+
+        chantier_order = self.env['chantier'].browse(doc.id).order_id
+        order_lines = chantier_order.order_line
+
+        for ol in order_lines:
+
+            if ol.product_id.categ_id.name == "Végétaux":
+                sum_v = sum_v + ol.price_unit
+                sum_c_v = sum_c_v + ol.purchase_price
+
+            elif ol.product_id.categ_id.name == "Préparation/Déplacement":
+                sum_p_d = sum_p_d + ol.price_unit
+                sum_c_p_d = sum_c_p_d + ol.purchase_price
+
+            elif ol.product_id.categ_id.name == "Tonte":
+                sum_t = sum_t + ol.price_unit
+                sum_c_t = sum_c_t + ol.purchase_price
+
+            elif ol.product_id.categ_id.name == "Taille":
+                sum_ta = sum_ta + ol.price_unit
+                sum_c_ta = sum_c_ta + ol.purchase_price
+
+            elif ol.product_id.categ_id.name == "Plantation":
+                sum_pl = sum_pl + ol.price_unit
+                sum_c_pl = sum_c_pl + ol.purchase_price
+
+            elif ol.product_id.categ_id.name == "Desherbage":
+                sum_dh = sum_dh + ol.price_unit
+                sum_c_dh = sum_c_dh + ol.purchase_price
+
+            elif ol.product_id.categ_id.name == "Protection":
+                sum_pr = sum_pr + ol.price_unit
+                sum_c_pr = sum_c_pr + ol.purchase_price
+
+            elif ol.product_id.categ_id.name == "Gazons":
+                sum_g = sum_g + ol.price_unit
+                sum_c_g = sum_c_g + ol.purchase_price
+
+            elif ol.product_id.categ_id.name == "Arrosage":
+                sum_a = sum_a + ol.price_unit
+                sum_c_a = sum_c_a + ol.purchase_price
+
+            elif ol.product_id.categ_id.name == "Fournitures plantation":
+                sum_f = sum_f + ol.price_unit
+                sum_c_f = sum_c_f + ol.purchase_price
+
+            elif ol.product_id.categ_id.name == "Sous-Traitance":
+                sum_s = sum_s + ol.price_unit
+                sum_c_s = sum_c_s + ol.purchase_price
+
+            else:
+                sum_divers = sum_divers + ol.price_unit
+                sum_c_divers = sum_c_divers + ol.purchase_price
+
+        sum = sum_v + sum_p_d + sum_t + sum_ta + sum_pl + sum_dh + sum_pr + sum_g + sum_a + sum_divers + sum_f + sum_s
+        sum_c = sum_c_v + sum_c_p_d + sum_c_t + sum_c_ta + sum_c_pl + sum_c_dh + sum_c_pr + sum_c_g + sum_c_a + sum_c_divers + sum_c_f + sum_c_s
+
+
+        return (sum_v,
+                sum_p_d,
+                sum_t,
+                sum_ta,
+                sum_pl,
+                sum_dh,
+                sum_pr,
+                sum_g,
+                sum_a,
+                sum_divers,
+                sum_f,
+                sum_s,
+
+                sum_c_v,
+                sum_c_p_d,
+                sum_c_t,
+                sum_c_ta,
+                sum_c_pl,
+                sum_c_dh,
+                sum_c_pr,
+                sum_c_g,
+                sum_c_a,
+                sum_c_divers,
+                sum_c_f,
+                sum_c_s,
+                sum,
+                sum_c)
+
+    @api.multi
     def get_time(self, heure):
         return {
             '7:00': 7,
@@ -106,14 +222,14 @@ class BilanReport(models.AbstractModel):
                     tmp_divers = tmp_divers + (self.get_time(et.heure_fin) - self.get_time(et.heure_deb))
 
         tmps.extend([tmp_p_d,
-                    tmp_t,
-                    tmp_ta,
-                    tmp_pl,
-                    tmp_dh,
-                    tmp_pr,
-                    tmp_g,
-                    tmp_a,
-                    tmp_divers])
+                     tmp_t,
+                     tmp_ta,
+                     tmp_pl,
+                     tmp_dh,
+                     tmp_pr,
+                     tmp_g,
+                     tmp_a,
+                     tmp_divers])
         return tmps
 
     @api.multi
@@ -134,33 +250,39 @@ class BilanReport(models.AbstractModel):
         for ets in etss:
             for et in ets:
                 if et.type == "p" or et.type == "d":
-                    tmp_cost_p_d = tmp_cost_p_d + (self.get_time(et.heure_fin) - self.get_time(et.heure_deb))*et.time_cost
+                    tmp_cost_p_d = tmp_cost_p_d + (self.get_time(et.heure_fin) - self.get_time(
+                        et.heure_deb)) * et.time_cost
                 elif et.type == "t":
-                    tmp_cost_t = tmp_cost_t + (self.get_time(et.heure_fin) - self.get_time(et.heure_deb))*et.time_cost
+                    tmp_cost_t = tmp_cost_t + (self.get_time(et.heure_fin) - self.get_time(et.heure_deb)) * et.time_cost
                 elif et.type == "ta":
-                    tmp_cost_ta = tmp_cost_ta + (self.get_time(et.heure_fin) - self.get_time(et.heure_deb))*et.time_cost
+                    tmp_cost_ta = tmp_cost_ta + (self.get_time(et.heure_fin) - self.get_time(
+                        et.heure_deb)) * et.time_cost
                 elif et.type == "pl":
-                    tmp_cost_pl = tmp_cost_pl + (self.get_time(et.heure_fin) - self.get_time(et.heure_deb))*et.time_cost
+                    tmp_cost_pl = tmp_cost_pl + (self.get_time(et.heure_fin) - self.get_time(
+                        et.heure_deb)) * et.time_cost
                 elif et.type == "dh":
-                    tmp_cost_dh = tmp_cost_dh + (self.get_time(et.heure_fin) - self.get_time(et.heure_deb))*et.time_cost
+                    tmp_cost_dh = tmp_cost_dh + (self.get_time(et.heure_fin) - self.get_time(
+                        et.heure_deb)) * et.time_cost
                 elif et.type == "pr":
-                    tmp_cost_pr = tmp_cost_pr + (self.get_time(et.heure_fin) - self.get_time(et.heure_deb))*et.time_cost
+                    tmp_cost_pr = tmp_cost_pr + (self.get_time(et.heure_fin) - self.get_time(
+                        et.heure_deb)) * et.time_cost
                 elif et.type == "g":
-                    tmp_cost_g = tmp_cost_g + (self.get_time(et.heure_fin) - self.get_time(et.heure_deb))*et.time_cost
+                    tmp_cost_g = tmp_cost_g + (self.get_time(et.heure_fin) - self.get_time(et.heure_deb)) * et.time_cost
                 elif et.type == "a":
-                    tmp_cost_a = tmp_cost_a + (self.get_time(et.heure_fin) - self.get_time(et.heure_deb))*et.time_cost
+                    tmp_cost_a = tmp_cost_a + (self.get_time(et.heure_fin) - self.get_time(et.heure_deb)) * et.time_cost
                 else:
-                    tmp_cost_divers = tmp_cost_divers + (self.get_time(et.heure_fin) - self.get_time(et.heure_deb))*et.time_cost
+                    tmp_cost_divers = tmp_cost_divers + (self.get_time(et.heure_fin) - self.get_time(
+                        et.heure_deb)) * et.time_cost
 
             tmp_costs.extend([tmp_cost_p_d,
-                         tmp_cost_t,
-                         tmp_cost_ta,
-                         tmp_cost_pl,
-                         tmp_cost_dh,
-                         tmp_cost_pr,
-                         tmp_cost_g,
-                         tmp_cost_a,
-                         tmp_cost_divers])
+                              tmp_cost_t,
+                              tmp_cost_ta,
+                              tmp_cost_pl,
+                              tmp_cost_dh,
+                              tmp_cost_pr,
+                              tmp_cost_g,
+                              tmp_cost_a,
+                              tmp_cost_divers])
             return tmp_costs
 
     @api.multi
@@ -202,6 +324,7 @@ class BilanReport(models.AbstractModel):
             'doc_model': report.model,
             'docs': self.env['chantier'].browse(self._ids),
             'bilan_main_oeuvre': self.bilan_main_oeuvre,
+            'get_sum_product' : self.get_sum_product
         }
 
         return self.env['report'].render('darb_puthod.report_bilan_chantier', docargs)
